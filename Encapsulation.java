be4f44c9e61f1b0966d60c75a95c9e26987b9a8e
Adam Alrasi <adam.alrasi@outlook.com>
2024-02-12 10:22:09 +0000

    Encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,10 @@
+/*
+Objectives:
+- Explain the OOP principle of encapsulation
+- Be able to give an example of encapsulation in the real world
+- Understand how encapsulation applies to programming
+
+Encapsulation is the mechanism of hiding of data implementation by restricting access to public methods. Instance variables are kept private and accessor methods are made public to achieve this.
+
+
+ */
